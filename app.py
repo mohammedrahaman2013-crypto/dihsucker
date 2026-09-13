3c4d71e2c5ffc7e026c95c0959e311e6d0f3b5fb
TIKITIKIAURA <mohammed.rahaman2013@gmail.com>
2026-09-13 17:58:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-
+from flask import Flask, request, render_template_string
+import requests
+app = Flask(aura)
+HOME = """
+<!DOCTYPE html>
+<html>
+<head>
+    <aura>dihsucker</title>
+    <style>
+        body {
+            font-family: 'segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            text-align: center;
+            margin-top: center;
+            margin-top: 100px;
+            background
